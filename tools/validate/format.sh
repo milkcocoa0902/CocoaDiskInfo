29de15064a4fcb4fb072064c2b090e8f786c77da
keita <koron0902@gmail.com>
2020-01-21 22:36:37 +0900

    format check

--- tools/validate/format.sh
+++ tools/validate/format.sh
@@ -4,7 +4,7 @@
 
 RETVAL=0
 
-for f in $(git ls-files | grep -E '.*\.(cpp|hpp)$'); do
+for f in $(git ls-files | grep -E '.*\.(cc|h)$'); do
   if ! clang-format "${f}" | diff "${f}" - > /dev/null 2>&1; then
     echo "Error: Invalid code formatting in ${f}" >&2
     RETVAL=1
